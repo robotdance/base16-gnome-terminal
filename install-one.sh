8923f3920b30da02c4651a18ea3511a0d4d7fdb0
MarcosMenegazzo <correiodomarcos@gmail.com>
2016-07-08 18:37:41 -0300

    Created install one color profile

---
+++ install-one.sh
@@ -0,0 +1,133 @@
+#!/usr/bin/env bash
+# Installs one base-16 profile
+# Marcos Menegazzo at robotdance
+#
+# $1 profile file name
+
+PROFILE=$1
+
+if test -r "$PROFILE" -a -f "$PROFILE"; then
+  source $PROFILE
+else
+  echo "Syntax: install-one.sh profile-path"
+  echo "(check also if you provided a readable file)"
+  exit 0
+fi
+
+[[ -z "$DCONF" ]] && DCONF=dconf
+[[ -z "$UUIDGEN" ]] && UUIDGEN=uuidgen
+
+dset() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    if [[ "$type" == "string" ]]; then
+        val="'$val'"
+    fi
+
+    "$DCONF" write "$PROFILE_KEY/$key" "$val"
+}
+
+# because dconf still doesn't have "append"
+dlist_append() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    local entries="$(
+        {
+            "$DCONF" read "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+            echo "'$val'"
+        } | head -c-1 | tr "\n" ,
+    )"
+
+    "$DCONF" write "$key" "[$entries]"
+}
+
+# Newest versions of gnome-terminal use dconf
+if which "$DCONF" > /dev/null 2>&1; then
+    [[ -z "$BASE_KEY_NEW" ]] && BASE_KEY_NEW=/org/gnome/terminal/legacy/profiles:
+
+    if [[ -n "`$DCONF list $BASE_KEY_NEW/`" ]]; then
+        if which "$UUIDGEN" > /dev/null 2>&1; then
+            PROFILE_SLUG=`uuidgen`
+        fi
+
+        if [[ -n "`$DCONF read $BASE_KEY_NEW/default`" ]]; then
+            DEFAULT_SLUG=`$DCONF read $BASE_KEY_NEW/default | tr -d \'`
+        else
+            DEFAULT_SLUG=`$DCONF list $BASE_KEY_NEW/ | grep '^:' | head -n1 | tr -d :/`
+        fi
+
+        DEFAULT_KEY="$BASE_KEY_NEW/:$DEFAULT_SLUG"
+        PROFILE_KEY="$BASE_KEY_NEW/:$PROFILE_SLUG"
+
+        # copy existing settings from default profile
+        $DCONF dump "$DEFAULT_KEY/" | $DCONF load "$PROFILE_KEY/"
+
+        # add new copy to list of profiles
+        dlist_append $BASE_KEY_NEW/list "$PROFILE_SLUG"
+
+        # update profile values with theme options
+        dset visible-name "'$PROFILE_NAME'"
+        dset background-color $BACKGROUND_COLOR
+        dset foreground-color $FOREGROUND_COLOR
+        dset bold-color $BOLD_COLOR
+        dset bold-color-same-as-fg $BOLD_COLOR_SAME_AS_FG
+        dset use-theme-colors $USE_THEME_COLORS
+        dset use-theme-background $USE_THEME_BACKGROUND
+        dset palette $PALETTE
+
+        unset PROFILE_NAME
+        unset PROFILE_SLUG
+        unset DCONF
+        unset UUIDGEN
+        #exits0
+    fi
+fi
+
+# Fallback for Gnome 2 and early Gnome 3
+[[ -z "$GCONFTOOL" ]] && GCONFTOOL=gconftool
+[[ -z "$BASE_KEY" ]] && BASE_KEY=/apps/gnome-terminal/profiles
+
+PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
+
+gset() {
+    local type="$1"; shift
+    local key="$1"; shift
+    local val="$1"; shift
+
+    "$GCONFTOOL" --set --type "$type" "$PROFILE_KEY$key" -- "$val"
+}
+
+# Because gconftool doesn't have "append"
+glist_append() {
+    local type="$1"; shift
+    local key="$1"; shift
+    local val="$1"; shift
+
+    local entries="$(
+        {
+            "$GCONFTOOL" --get "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+            echo "$val"
+        } | head -c-1 | tr "\n" ,
+    )"
+
+    "$GCONFTOOL" --set --type list --list-type $type "$key" "[$entries]"
+}
+
+# Append the Base16 profile to the profile list
+glist_append string /apps/gnome-terminal/global/profile_list "$PROFILE_SLUG"
+
+gset string visible_name "$PROFILE_NAME"
+gset string palette $PALETTE
+gset string background_color $BACKGROUND_COLOR
+gset string foreground_color $FOREGROUND_COLOR
+gset string bold_color $BOLD_COLOR
+gset bool   bold_color_same_as_fg $BOLD_COLOR_SAME_AS_FG
+gset bool   use_theme_colors $USE_THEME_COLORS
+gset bool   use_theme_background $USE_THEME_BACKGROUND
+
+unset PROFILE_NAME
+unset PROFILE_SLUG
+unset DCONF
+unset UUIDGEN
