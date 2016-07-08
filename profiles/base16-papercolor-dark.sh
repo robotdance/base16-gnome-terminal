88d375abea5a8bce25d2b74ad730be5a6892d3f6
MarcosMenegazzo <correiodomarcos@gmail.com>
2016-07-08 19:07:37 -0300

    Added papercolor theme

---
+++ profiles/base16-papercolor-dark.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Base16 PaperColor-Dark - Gnome Terminal color scheme install script
+# Nguyen Nguyen (http://github.com/NLKNguyen)
+
+PROFILE_NAME="Base 16 PaperColor Dark"
+PROFILE_SLUG="base-16-papercolor-dark"
+PALETTE="'#262626:#af87af:#dfaf5f:#00afaf:#df875f:#afdf00:#5fafdf:#d0d0d0:#808080:#af87af:#dfaf5f:#00afaf:#df875f:#afdf00:#5fafdf:#f3f3f3'"
+BACKGROUND_COLOR="'#262626'"
+FOREGROUND_COLOR="'#d0d0d0'"
+BOLD_COLOR="'#d0d0d0'"
+BOLD_COLOR_SAME_AS_FG="true"
+USE_THEME_COLORS="false"
+USE_THEME_COLORS="false"
---
+++ profiles/base16-papercolor-light.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Base16 PaperColor-Dark - Gnome Terminal color scheme install script
+# Nguyen Nguyen (http://github.com/NLKNguyen)
+
+PROFILE_NAME="Base 16 PaperColor Light"
+PROFILE_SLUG="base-16-papercolor-light"
+PALETTE="'#262626:#af87af:#dfaf5f:#00afaf:#df875f:#afdf00:#5fafdf:#d0d0d0:#808080:#af87af:#dfaf5f:#00afaf:#df875f:#afdf00:#5fafdf:#f3f3f3'"
+PALETTE="'#f3f3f3:#8959a8:#718c00:#4271ae:#005f87:#d7005f:#3e999f:#d0d0d0:#808080:#8959a8:#718c00:#4271ae:#005f87:#d7005f:#3e999f:#262626'"
+BACKGROUND_COLOR="'#f3f3f3'"
+FOREGROUND_COLOR="'#4d4d4c'"
+BOLD_COLOR="'4d4d4c'"
+BOLD_COLOR_SAME_AS_FG="true"
+USE_THEME_COLORS="false"
+USE_THEME_COLORS="false"
